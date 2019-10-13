e67f4a8ce86fa15509f5c6c49cb200d9d948a25a
Marmelade <tjulmd@qq.com>
2019-10-13 14:25:10 +0800

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,6 @@
 python detector/to_img.py
 
-python detector/yolov3/detect.py --source result/img/b1/img1 --output result/img/b1/det --img-size 320 --save_txt
+python detector/yolov3/detect.py --source result/img/b1/img1 --output result/img/b1/det --img-size 1920 --save_txt
 python detector/yolov3/detect.py --source result/img/b2/img1 --output result/img/b2/det --img-size 1920 --save_txt
 python detector/yolov3/detect.py --source result/img/b3/img1 --output result/img/b3/det --img-size 608 --save_txt
 python detector/yolov3/detect.py --source result/img/b5/img1 --output result/img/b5/det --img-size 1024 --save_txt
